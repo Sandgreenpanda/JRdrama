be4bb80b83ab2d1d71ddae1da76a2f2b6ec97881
sandgreenpanda <23123@wc.school.nz>
2026-03-19 20:26:14 +0000

    Initial commit

---
+++ static/main.js
@@ -0,0 +1,24 @@
+const date = document.getElementById("date");
+
+
+if (date) {
+    date.addEventListener("change", (event) => {
+    let today_attendance = record[date.value];
+    console.log(today_attendance);
+    console.log(date.value);
+
+    console.log("HI");
+    let i = 0;
+    for (const item in people) {
+        let here = Boolean(today_attendance[i][1]);
+        i++;
+        const person = document.getElementById(i);
+        console.log(i);
+        if (person) {
+
+
+        person.checked = here;
+        }
+    }
+    });
+}
